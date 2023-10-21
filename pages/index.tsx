42dc2ce3baa47e04c6fc1ea23660953143ea05b4
khanhla <khanhla@runsystem.net>
2023-10-21 10:37:43 +0700

    first

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>UPDATE</title>
+        <title>UPDATE 12123123</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
